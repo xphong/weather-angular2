818a31514ad516b85f2aa2ea980d0e2758fe8311
Phong Huynh <xphong@gmail.com>
2016-02-11 14:29:41 -0500

    Get and display weather forecast

--- src/app/shared/services/weather.ts
+++ src/app/shared/services/weather.ts
@@ -3,16 +3,18 @@ import {Http} from 'angular2/http';
 
 @Injectable()
 export class WeatherService {
+  apiUrl = 'http://api.openweathermap.org/data/2.5/forecast/daily';
+  apiKey = '0f39b39c55f9f764c41360fbe41282b7';
+
   constructor(public http: Http) {
 
   }
 
-  getData() {
-    // return this.http.get('/assets/data.json')
-    // .map(res => res.json());
-    return {
-      value: 'AngularClass'
-    };
+  getForecast(city) {
+    let url = this.apiUrl + '?q=' + city + '&cnt=7&mode=json&units=metric&appid=' + this.apiKey;
+
+    return this.http.get(url)
+                    .map(res => res.json());
   }
 
 }
