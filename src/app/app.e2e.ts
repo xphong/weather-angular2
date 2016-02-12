59f1864ab5d1f1ff7b58f3b7a0d5c43c75dd711f
Phong Huynh <xphong@gmail.com>
2016-02-12 15:36:46 -0500

    Update protractor test for app

--- src/app/app.e2e.ts
+++ src/app/app.e2e.ts
@@ -4,15 +4,14 @@ describe('App', () => {
     browser.get('/');
   });
 
-
   it('should have a title', () => {
     let subject = browser.getTitle();
     let result  = 'Weather App';
     expect(subject).toEqual(result);
   });
 
-  it('should have <header>', () => {
-    let subject = element(by.css('app header')).isPresent();
+  it('should have <nav>', () => {
+    let subject = element(by.css('app nav')).isPresent();
     let result  = true;
     expect(subject).toEqual(result);
   });
