71fbac37164dbdb15748efcdb777751c08aeaac7
Phong Huynh <xphong@gmail.com>
2016-02-18 14:49:22 -0500

    Protractor tests for home and city components

--- src/app/city/city.e2e.ts
+++ src/app/city/city.e2e.ts
@@ -10,4 +10,25 @@ describe('App', () => {
     expect(subject).toEqual(result);
   });
 
+  it('should have <city>', () => {
+    let subject = element(by.css('app city')).isPresent();
+    let result  = true;
+    expect(subject).toEqual(result);
+  });
+
+  it('should have a heading with the city name', () => {
+    let subject = element(by.css('h2')).getText();
+    let result  = 'TORONTO';
+    expect(subject).toEqual(result);
+  });
+
+  it('should show table with forecast', () => {
+    let result  = true;
+
+    browser.sleep(500);
+
+	  browser.findElement(by.css('.table')).then((el) => {
+      expect(el.isDisplayed()).toBe(result);
+    });
+  });
 });
--- src/app/home/home.e2e.ts
+++ src/app/home/home.e2e.ts
@@ -10,4 +10,71 @@ describe('App', () => {
     expect(subject).toEqual(result);
   });
 
+  it('should have input text box', () => {
+    let subject = element(by.css('.weather input[type=text]')).isPresent();
+    let result  = true;
+    expect(subject).toEqual(result);
+  });
+
+  it('should have submit button', () => {
+    let subject = element(by.css('.weather button[type=submit]')).isPresent();
+    let result  = true;
+    expect(subject).toEqual(result);
+  });
+
+  it('should display error message for forecast not found', () => {
+    let textBox = element(by.css('.weather input[type=text]'));
+    let submitButton = element(by.css('.weather button[type=submit]'));
+    let table;
+
+    textBox.clear();
+    textBox.sendKeys('12313211231321').then(() => {
+      submitButton.click();
+
+      browser.sleep(1000);
+
+      browser.findElement(by.css('.help-block')).then((el) => {
+        expect(el.isDisplayed()).toBe(true);
+      });
+
+      table = element(by.css('.table')).isPresent();
+      expect(table).toEqual(false)
+    });
+  });
+
+  it('should search city for forecast on click', () => {
+    let textBox = element(by.css('.weather input[type=text]'));
+    let submitButton = element(by.css('.weather button[type=submit]'));
+    let result  = true;
+
+    textBox.clear();
+    textBox.sendKeys('Toronto').then(() => {
+      submitButton.click();
+
+      browser.sleep(500);
+
+      browser.findElement(by.css('.table')).then((el) => {
+        expect(el.isDisplayed()).toBe(result);
+      });
+    });
+
+  });
+
+  it('should search city for forecast on enter', () => {
+    let textBox = element(by.css('.weather input[type=text]'));
+    let submitButton = element(by.css('.weather button[type=submit]'));
+    let result  = true;
+
+    textBox.clear();
+    textBox.sendKeys('Toronto').then(() => {
+      textBox.sendKeys(protractor.Key.ENTER);
+
+      browser.sleep(500);
+
+      browser.findElement(by.css('.table')).then((el) => {
+        expect(el.isDisplayed()).toBe(result);
+      });
+    });
+  });
+
 });
